532b5210087abfd382f89b4d1644ebb02a60e0a7
nicofilliol <nicolas.filliol@outlook.com>
2020-04-30 17:06:45 +0200

    optimize performance using numpy

--- fourier.py
+++ fourier.py
@@ -3,9 +3,9 @@ import numpy as np
 import numexpr as ne
 import matplotlib.pyplot as plt
 import math, cmath
+import time as tm
 
-#TODO 1) Vectorize calucaltions (very slow at the moment)
-#TODO 2) Implement real fourier series
+#TODO Implement real fourier series
 
 def complex_fourier(c, N, t, T=math.pi*2):
     """Calculates value of f(t) with the help of its complex fourier series approximation.
@@ -13,7 +13,7 @@ def complex_fourier(c, N, t, T=math.pi*2):
     Arguments:
         c {function/lambda} -- function expecting n as parameter and returning n-th coefficient (c_n * exp(i*2π/T*nt))
         N {int} -- upper limit of sum, the higher N the better the approximation 
-        t {double} -- the point where the series should be evaluated
+        t {np.array} -- the points where the series should be evaluated
         T {double} -- period of function (default: {2π})
 
     Returns:
@@ -21,18 +21,21 @@ def complex_fourier(c, N, t, T=math.pi*2):
     """ 
     w = 2 * math.pi / T # calculate angular frequency
 
+    sum = np.zeros(t.shape,dtype=np.complex128)
     c_0 = c(0) # get first coeffiecient
-    sum = c_0
+    sum = sum + c_0
 
     for n in range(1, N+1):
+        # Get coefficients for e^inwt and e^-inwt
         c_n = c(n)
         c_nn = c(-n)
 
-        sum = sum + c_n * cmath.exp(1j*n*w*t)
-        sum = sum + c_nn * cmath.exp(-1j*n*w*t)
+        # Vectorize calculation with numpy to improve performance
+        exponent = 1j * n * w * t
+        sum = sum + c_n * np.exp(exponent)
+        sum = sum + c_nn * np.exp(-exponent)
 
-    #assert(sum.imag == 0) # make sure that sum has no imaginary component (for real functions)
-    return sum.real
+    return np.real(sum)
 
 def real_fourier(a, b, N, t):
     """Calculates value of f(t) with the help of its real fourier series approximation.
@@ -61,24 +64,6 @@ def real_fourier(a, b, N, t):
         sum += b_n * math.sin(n*w*t)
     
     return sum
-
-def generate_complex_data(c, N, interval):
-    """Calculates f(t) for provided interval using complex fourier series.
-
-    Arguments:
-        c {function/lambda} -- function expecting n as parameter and returning n-th coefficient (c_n * exp(i*2π/T*nt))
-        N {int} -- upper limit of sum, the higher N the better the approximation 
-        time_interval {list} -- list of input values
-
-    Returns:
-        np.array -- returns array of evaluated points f(t)
-    """
-    data = np.zeros(len(interval))
-
-    for i, t in enumerate(interval):
-        data[i] = complex_fourier(c, N, t)
-    
-    return data
 
 
 def check_input(values):
@@ -124,15 +109,15 @@ def main():
     c = lambda n: ne.evaluate(c_n) if n != 0 else c_0 
 
     # Create input list, calculate output and plot data
-    time = np.linspace(interval[0], interval[1], 1000)
+    time = np.linspace(interval[0], interval[1], 5000)
     fig, axs = plt.subplots(3, 2, num="Fourier Series")
 
     for i, N in enumerate([1, 2, 5, 10, 100, 1000]):
-        y = generate_complex_data(c, N, time)
+        y = complex_fourier(c, N, time)
 
         col = i % 2
         row = i//2 
-        axs[row, col].plot(time, y)
+        axs[row, col].plot(time, y, linewidth=1.5)
         axs[row, col].set_title(f'N = {N}')
 
     fig.tight_layout()
